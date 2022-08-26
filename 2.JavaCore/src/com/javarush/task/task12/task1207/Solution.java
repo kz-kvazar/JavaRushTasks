dc4a21c798f4997dd7b5815a8e5449384b6e8f7b
kz-kvazar <milniy87@gmail.com>
2022-08-26 21:14:01 +0300

    Test

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+print(10);
+Integer i = 10;
+print(i);
+    }
+public static void print(int i){}
+    public static void print(Integer i){}
+    //Напишите тут ваши методы
+}
