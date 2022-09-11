b8685377d1ead33c7b4cf4f77f135560e687d483
kz-kvazar <milniy87@gmail.com>
2022-09-11 08:32:12 +0300

    Test

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution(int i) {
+    }
+
+    protected Solution(long l) {
+    }
+
+    Solution(double d) {
+    }
+
+    private Solution(char c) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
