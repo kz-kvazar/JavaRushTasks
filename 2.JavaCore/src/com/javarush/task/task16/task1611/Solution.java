1ce4ee943c1af3d9880b1b8a5accc306081f6129
kz-kvazar <milniy87@gmail.com>
2022-09-12 23:31:53 +0300

    Test

--- 2.JavaCore/src/com/javarush/task/task16/task1611/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1611/Solution.java
@@ -34,10 +34,10 @@ public class Solution {
 
         private void printTikTak() throws InterruptedException {
             //add your code here - добавь код тут
+            Thread.sleep(500);
             System.out.println("Tik");
             Thread.sleep(500);
             System.out.println("Tak");
-            Thread.sleep(500);
         }
     }
 }
