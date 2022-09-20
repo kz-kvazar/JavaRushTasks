c01c7c1b1bb4871390781d85a79ea78b208d2de2
kz-kvazar <milniy87@gmail.com>
2022-09-20 23:12:50 +0300

    Test

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task17.task1714;
+
+/* 
+Comparable
+*/
+
+public class Beach implements Comparable<Beach> {
+    private String name;      //название
+    private float distance;   //расстояние
+    private int quality;    //качество
+
+    public Beach(String name, float distance, int quality) {
+        this.name = name;
+        this.distance = distance;
+        this.quality = quality;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public synchronized String getName() {
+        return name;
+    }
+
+    public synchronized void setName(String name) {
+        this.name = name;
+    }
+
+    public synchronized float getDistance() {
+        return distance;
+    }
+
+    public synchronized void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public synchronized int getQuality() {
+        return quality;
+    }
+
+    public synchronized void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public synchronized int compareTo(Beach o) {
+        int design = 0;
+        if (this.getQuality() > o.getQuality()) {
+            design++;
+        } else if (this.getQuality() < o.getQuality()) {
+            design--;
+        }
+        if (this.getDistance() > o.getDistance()) {
+            design--;
+        } else if (this.getDistance() < o.getDistance()) {
+            design++;
+        }
+        return design;
+    }
+}
