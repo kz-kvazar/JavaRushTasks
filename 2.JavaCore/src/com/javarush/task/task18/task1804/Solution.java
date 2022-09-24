1601845225df855911bb9d39dd46cc3d74ea0c1a
kz-kvazar <milniy87@gmail.com>
2022-09-24 10:34:29 +0300

    Test

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -3,7 +3,6 @@ package com.javarush.task.task18.task1804;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 /*
 Самые редкие байты
@@ -22,12 +21,10 @@ public class Solution {
         }
         int minCount = Integer.MAX_VALUE;
         for (int byteCount : byteCountArray) {
-            if (byteCount > 0 && byteCount < minCount) minCount = byteCount;
+            if ( byteCount < minCount) minCount = byteCount;
         }
-        ArrayList<Integer> resultList = new ArrayList<>();
         for (int i = 0; i < byteCountArray.length; i++) {
-            if (byteCountArray[i] == minCount) resultList.add(i);
+            if (byteCountArray[i] == minCount) System.out.print(i + " ");
         }
-        for (Integer resultItem : resultList) System.out.print(resultItem + " ");
     }
 }
