eb17b2ed588680dde2ee5970d97306f81c3c4463
kz-kvazar <milniy87@gmail.com>
2022-09-28 21:32:30 +0300

    Test

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task18.task1820;
+
+import java.io.*;
+
+/* 
+Округление чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String filename2 = reader.readLine();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName1));
+             PrintWriter fileWriter = new PrintWriter(new FileWriter(filename2))) {
+            while (fileReader.ready()) {
+                String[] strings = fileReader.readLine().split(" ");
+                for (String s : strings) {
+                    double d = Double.parseDouble(s);
+                    long l = Math.round(d);
+                    fileWriter.print(l + " ");
+                }
+            }
+        }
+    }
+}
