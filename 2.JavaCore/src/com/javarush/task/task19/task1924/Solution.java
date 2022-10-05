c4bdfde700504a465e253442e7e412335522e5ad
kz-kvazar <milniy87@gmail.com>
2022-10-05 17:02:30 +0300

    Test

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1924/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task19.task1924;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Замена чисел
+*/
+
+public class Solution {
+    public static Map<Integer, String> map = new HashMap<>();
+
+    static {
+        map.put(12, "двенадцать");
+        map.put(11, "одиннадцать");
+        map.put(10, "десять");
+        map.put(9, "девять");
+        map.put(8, "восемь");
+        map.put(7, "семь");
+        map.put(6, "шесть");
+        map.put(5, "пять");
+        map.put(4, "четыре");
+        map.put(3, "три");
+        map.put(2, "два");
+        map.put(1, "один");
+        map.put(0, "ноль");
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        reader.close();
+        try (BufferedReader fileReadr = new BufferedReader(new FileReader(file))) {
+            while (fileReadr.ready()) {
+                String line = fileReadr.readLine();
+                for (Map.Entry<Integer, String> value : map.entrySet()) {
+                    line = line.replaceAll("\\b" + value.getKey() + "\\b", value.getValue());
+                }
+                System.out.println(line);
+            }
+        }
+    }
+}
