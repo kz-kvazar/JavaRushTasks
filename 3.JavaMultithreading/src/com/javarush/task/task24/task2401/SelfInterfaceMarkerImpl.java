4128d632f7ceb476c83bde904bfdef8bc26df4a2
kz-kvazar <milniy87@gmail.com>
2022-11-26 16:23:32 +0200

    Test

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void printHi(){
+        System.out.println("hi");
+    }
+    public void printHello(){
+        System.out.println("hello");
+    }
+}
