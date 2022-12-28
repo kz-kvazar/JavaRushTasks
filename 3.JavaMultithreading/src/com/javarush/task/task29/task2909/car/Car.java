5ca3c92e978099c8019877de0c7e3cf81e889415
kz-kvazar <milniy87@gmail.com>
2022-12-28 14:11:38 +0200

    Test

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -2,55 +2,61 @@ package com.javarush.task.task29.task2909.car;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
     static public final int TRUCK = 0;
     static public final int SEDAN = 1;
     static public final int CABRIOLET = 2;
-
-    double fuel;
-
+    public static final int MAX_TRUCK_SPEED = 80;
+    public static final int MAX_SEDAN_SPEED = 120;
+    public static final int MAX_CABRIOLET_SPEED = 90;
     public double summerFuelConsumption;
     public double winterFuelConsumption;
     public double winterWarmingUp;
-
-    private int type;
+    double fuel;
+    private final int type;
 
     private boolean driverAvailable;
-    private int numberOfPassengers;
+    private final int numberOfPassengers;
 
     protected Car(int type, int numberOfPassengers) {
         this.type = type;
         this.numberOfPassengers = numberOfPassengers;
     }
-    public static Car create(int type, int numberOfPassengers){
-        switch (type){
-            case 0: return new Truck(numberOfPassengers);
-            case  1: return new Sedan(numberOfPassengers);
-            case 2: return new Cabriolet(numberOfPassengers);
+
+    public static Car create(int type, int numberOfPassengers) {
+        switch (type) {
+            case 0:
+                return new Truck(numberOfPassengers);
+            case 1:
+                return new Sedan(numberOfPassengers);
+            case 2:
+                return new Cabriolet(numberOfPassengers);
             default:
                 throw new IllegalStateException("Unexpected value: " + type);
         }
     }
 
-    public void fill(double numberOfLiters) throws Exception {
-        if (numberOfLiters < 0)
-            throw new Exception();
-        fuel += numberOfLiters;
-    }
-    public boolean isSummer(Date date, Date summerStart, Date summerEnd){
-        if (date.after(summerStart) && date.before(summerEnd)){
-            return true;
-        } else {
-            return false;
-        }
+    private boolean canPassengersBeTransferred() {
+        return isDriverAvailable() && fuel > 0;
     }
 
-    public double getWinterConsumption(int length){
+    public void fill(double numberOfLiters) throws Exception {
+        if (numberOfLiters < 0) throw new Exception();
+        fuel += numberOfLiters;
+    }
+
+    public boolean isSummer(Date date, Date summerStart, Date summerEnd) {
+        return date.after(summerStart) && date.before(summerEnd);
+    }
+
+    public double getWinterConsumption(int length) {
         return length * winterFuelConsumption + winterWarmingUp;
     }
-    public double getSummerConsumption(int length){
+
+    public double getSummerConsumption(int length) {
         return length * summerFuelConsumption;
     }
+
     public double getTripConsumption(Date date, int length, Date SummerStart, Date SummerEnd) {
         if (isSummer(date, SummerStart, SummerEnd)) {
             return getSummerConsumption(length);
@@ -60,12 +66,10 @@ public class Car {
     }
 
     public int getNumberOfPassengersCanBeTransferred() {
-        if (!isDriverAvailable())
-            return 0;
-        if (fuel <= 0)
-            return 0;
-
-        return numberOfPassengers;
+        if (canPassengersBeTransferred()) {
+            return numberOfPassengers;
+        }
+        return 0;
     }
 
     public boolean isDriverAvailable() {
@@ -79,10 +83,8 @@ public class Car {
     public void startMoving() {
         if (numberOfPassengers > 0) {
             fastenPassengersBelts();
-            fastenDriverBelt();
-        } else {
-            fastenDriverBelt();
         }
+        fastenDriverBelt();
     }
 
     public void fastenPassengersBelts() {
@@ -91,11 +93,5 @@ public class Car {
     public void fastenDriverBelt() {
     }
 
-    public int getMaxSpeed() {
-        if (type == TRUCK)
-            return 80;
-        if (type == SEDAN)
-            return 120;
-        return 90;
-    }
+    public abstract int getMaxSpeed();
 }
