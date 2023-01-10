94386b17b59e3c273d76c65161a0d32d028e6b12
kz-kvazar <milniy87@gmail.com>
2023-01-11 01:23:12 +0200

    Test

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,13 +3,17 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) throws IOException {
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
 
-        try (ServerSocket serverSocket = new ServerSocket(port)){
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 Handler handler = new Handler(serverSocket.accept());
                 handler.start();
@@ -17,6 +21,16 @@ public class Server {
         } catch (Exception e) {
             ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
         }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        connectionMap.forEach((k, v) -> {
+            try {
+                v.send(message);
+            } catch (IOException e) {
+                System.out.println("Сообщение не было отправленно.");
+            }
+        });
     }
 
     private static class Handler extends Thread {
