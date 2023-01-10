94bdc0149342c126016b32774696424780a8191c
kz-kvazar <milniy87@gmail.com>
2023-01-10 20:52:13 +0200

    Test

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,4 +1,30 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+    public static String readString(){
+        try {
+            return bis.readLine();
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+           return readString();
+        }
+    }
+    public static int readInt(){
+        try{
+            return Integer.parseInt(readString());
+        } catch (NumberFormatException e)
+        {
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            return readInt();
+        }
+    }
 }
