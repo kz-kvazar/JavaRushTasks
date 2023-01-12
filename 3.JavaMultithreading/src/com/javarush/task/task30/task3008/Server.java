d872d225f43cd4ee190bf0e113bd566f36f7b4ee
kz-kvazar <milniy87@gmail.com>
2023-01-12 13:57:52 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -12,6 +12,7 @@ public class Server {
     public static void main(String[] args) throws IOException {
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
+        int i = 0;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
