67e195185957bc1914dbf69a489ae93c4385de79
kz-kvazar <milniy87@gmail.com>
2023-01-12 15:22:27 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Connection.java
@@ -9,23 +9,23 @@ import java.net.SocketAddress;
 
 public class Connection implements Closeable {
     private final Socket socket;
+    private final ObjectInputStream in;
     private final ObjectOutputStream out;
-
-    private  final ObjectInputStream in;
 
     public Connection(Socket socket) throws IOException {
         this.socket = socket;
-        out = new ObjectOutputStream(socket.getOutputStream());
-        in = new ObjectInputStream(socket.getInputStream());
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
     }
-    public void  send(Message message) throws IOException{
-       synchronized (out){
-           out.writeObject(message);
-           out.flush();
-       }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+        }
     }
-    public Message receive() throws IOException, ClassNotFoundException{
-        synchronized (in){
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (in) {
             return (Message) in.readObject();
         }
     }
--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -9,33 +9,24 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Server {
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
 
-
         try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Чат сервер запущен.");
             while (true) {
-                Handler handler = new Handler(serverSocket.accept());
-                handler.start();
+                // Ожидаем входящее соединение и запускаем отдельный поток при его принятии
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
             }
         } catch (Exception e) {
             ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
         }
     }
 
-    public static void sendBroadcastMessage(Message message) {
-        connectionMap.forEach((k, v) -> {
-            try {
-                v.send(message);
-            } catch (IOException e) {
-                System.out.println("Сообщение не было отправленно.");
-            }
-        });
-    }
-
     private static class Handler extends Thread {
-        private final Socket socket;
+        private Socket socket;
 
         public Handler(Socket socket) {
             this.socket = socket;
@@ -43,7 +34,40 @@ public class Server {
 
         @Override
         public void run() {
-            super.run();
+
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+                if (!message.getType().equals(MessageType.USER_NAME)) {
+                    ConsoleHelper.writeMessage("Неверный тип сообщения");
+                    continue;
+                }
+                if (message.getData().isEmpty()) {
+                    ConsoleHelper.writeMessage("Имя пользователя не может быть пустое");
+                    continue;
+                }
+                if (connectionMap.containsKey(message.getData())) {
+                    ConsoleHelper.writeMessage("Пользователь с таким именем уже существует");
+                    continue;
+                }
+                connectionMap.put(message.getData(), connection);
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                return message.getData();
+            }
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        // Рассылаем сообщение по всем соединениям
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Не смогли отправить сообщение " + connection.getRemoteSocketAddress());
+            }
         }
     }
 }
