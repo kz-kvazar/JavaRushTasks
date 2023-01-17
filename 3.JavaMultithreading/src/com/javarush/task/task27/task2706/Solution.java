ad1a8889daadcd0fd5e26aa7bc14bf29eff1d968
kz-kvazar <milniy87@gmail.com>
2023-01-17 16:42:16 +0200

    test

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2706/Solution.java
@@ -0,0 +1,56 @@
+package com.javarush.task.task27.task2706;
+
+/* 
+Убираем deadlock
+*/
+
+public class Solution {
+    public void safeMethod(Object obj1, Object obj2) {
+        Object object1;
+        Object object2;
+        if (obj1.hashCode() > obj2.hashCode()){
+            object1 = obj1;
+            object2 = obj2;
+        } else {
+            object1 = obj2;
+            object2 = obj1;
+        }
+        synchronized (object1) {
+            longTimeMethod();
+            synchronized (object2) {
+                unsafeMethod(obj1, obj2);
+            }
+        }
+    }
+
+    public void longTimeMethod() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    protected void unsafeMethod(Object obj1, Object obj2) {
+        System.out.println(obj1 + " " + obj2);
+    }
+
+    public static void main(String[] args) {
+        final Object o1 = new Object();
+        final Object o2 = new Object();
+        final Solution solution = new Solution();
+
+        new Thread() {
+            @Override
+            public void run() {
+                solution.safeMethod(o1, o2);
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                solution.safeMethod(o2, o1);
+            }
+        }.start();
+    }
+}
