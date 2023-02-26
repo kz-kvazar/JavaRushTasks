cb683ac29fe948fa0f66ad1729c8b757516e7256
kz-kvazar <milniy87@gmail.com>
2023-02-26 10:21:25 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
@@ -1,5 +1,11 @@
 package com.javarush.task.task28.task2811;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.LinkedHashMap;
 
 /* 
@@ -7,7 +13,17 @@ ReentrantReadWriteLock
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
+        Document page = Jsoup.connect("https://etnosvit.com/anekdoty.html")
+                .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                .referrer("http://www.google.com")
+                .get();
+        Elements elements = page.select("[class=sue-panel-content sue-content-wrap]");
+        //
+        for (int i = 3; i < 5; i++) {
+            Element element = elements.get(i);
+            System.out.println(element.text());
+        }
     }
 }
