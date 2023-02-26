57297d933a6cc5cd35c7b566e5e26dd6c4d4ec5f
kz-kvazar <milniy87@gmail.com>
2023-02-26 11:27:09 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,11 +1,15 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
@@ -21,12 +25,26 @@ public class View extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
     }
 
     public void init(){
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        setVisible(true);
 
     }
+    public void initMenuBar(){
+    }
+    public void initEditor(){
+
+    }
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
     public void exit(){
         controller.exit();
     }
