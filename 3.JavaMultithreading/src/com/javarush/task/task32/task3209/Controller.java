fe92bbe897df4214eaf69e8e74ac6a27d8f57e69
kz-kvazar <milniy87@gmail.com>
2023-02-28 09:44:25 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -19,7 +19,7 @@ public class Controller {
 
     public static void main(String[] args) {
         View view1 = new View();
-        Controller controller= new Controller(view1);
+        Controller controller = new Controller(view1);
 
         view1.setController(controller);
         view1.init();
@@ -31,8 +31,9 @@ public class Controller {
     public HTMLDocument getDocument() {
         return document;
     }
-    public void resetDocument(){
-        if (document != null){
+
+    public void resetDocument() {
+        if (document != null) {
             document.removeUndoableEditListener(view.getUndoListener());
         }
         document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
@@ -40,41 +41,74 @@ public class Controller {
         view.update();
     }
 
-    public void init(){
+    public void init() {
         createNewDocument();
     }
-    public void exit(){
+
+    public void exit() {
         System.exit(0);
     }
-    public void setPlainText(String text){
+
+    public void setPlainText(String text) {
         resetDocument();
         StringReader stringReader = new StringReader(text);
         try {
-            new HTMLEditorKit().read(stringReader,document,0);
+            new HTMLEditorKit().read(stringReader, document, 0);
         } catch (Exception e) {
             ExceptionHandler.log(e);
         }
     }
-    public String getPlainText(){
+
+    public String getPlainText() {
         StringWriter stringWriter = new StringWriter();
         try {
-            new HTMLEditorKit().write(stringWriter,document,0,document.getLength());
+            new HTMLEditorKit().write(stringWriter, document, 0, document.getLength());
         } catch (Exception e) {
             ExceptionHandler.log(e);
         }
         return stringWriter.toString();
     }
-    public void createNewDocument(){
+
+    public void createNewDocument() {
         view.selectHtmlTab();
         resetDocument();
-        view.setTitle("новый HTML файл");
+        view.setTitle("HTML редактор");
+        view.resetUndo();
         currentFile = null;
     }
 
     public void openDocument() {
+        view.selectHtmlTab();
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new HTMLFileFilter());
+        fileChooser.setDialogTitle("Open File");
+
+        if (fileChooser.showOpenDialog(view) == JFileChooser.APPROVE_OPTION){
+            currentFile = fileChooser.getSelectedFile();
+            resetDocument();
+            view.setTitle(currentFile.getName());
+            try (FileReader reader = new FileReader(currentFile)){
+                HTMLEditorKit editorKit = new HTMLEditorKit();
+                editorKit.read(reader,document,0);
+                view.resetUndo();
+            }catch (Exception e){
+                ExceptionHandler.log(e);
+            }
+        }
     }
 
     public void saveDocument() {
+        view.selectHtmlTab();
+        if (currentFile == null) {
+            saveDocumentAs();
+        } else {
+            try (FileWriter writer = new FileWriter(currentFile)) {
+                HTMLEditorKit editorKit = new HTMLEditorKit();
+                editorKit.write(writer, document, 0, document.getLength());
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+        }
     }
 
     public void saveDocumentAs() {
@@ -86,9 +120,9 @@ public class Controller {
         if (fileChooser.showSaveDialog(view) == JFileChooser.APPROVE_OPTION) {
             currentFile = fileChooser.getSelectedFile();
             view.setTitle(currentFile.getName());
-            try (FileWriter writer = new FileWriter(currentFile)){
+            try (FileWriter writer = new FileWriter(currentFile)) {
                 HTMLEditorKit editorKit = new HTMLEditorKit();
-                editorKit.write(writer,document,0,document.getLength());
+                editorKit.write(writer, document, 0, document.getLength());
             } catch (Exception e) {
                 ExceptionHandler.log(e);
             }
