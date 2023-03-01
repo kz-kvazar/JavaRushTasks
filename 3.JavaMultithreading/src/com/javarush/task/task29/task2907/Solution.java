251b25410f0fe51e3e8938145afe7b5e9acfd79c
kz-kvazar <milniy87@gmail.com>
2023-03-01 12:49:49 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2907/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2907/Solution.java
@@ -8,7 +8,9 @@ import java.math.BigDecimal;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(getValue(1.1d, 1.2d));
+        System.out.println(getValue(1.1d, 1.2d));    //2.3 expected
+        System.out.println(getValue(-1.1d, 1.1d));   //0.0 expected
+        System.out.println(getValue(.100d, 1.100d)); //1.2 expected
     }
 
     public static BigDecimal getValue(double v1, double v2) {
