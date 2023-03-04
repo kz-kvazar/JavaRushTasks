56e01af446ab5286a66235995d04d3b135256325
kz-kvazar <milniy87@gmail.com>
2023-03-04 08:01:55 +0200

    test

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ConsoleHelper.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task27.task2712;
+
+import com.javarush.task.task27.task2712.kitchen.Dish;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsoleHelper {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        return reader.readLine();
+    }
+
+    public static List<Dish> getAllDishesForOrder() throws IOException {
+        List<Dish> dishList = new ArrayList<>();
+        String order = "null";
+
+        while (!order.equals("exit")) {
+            writeMessage("Выберите блюдо для заказа или exit для выхода");
+            writeMessage(Dish.allDishesToString());
+            order = readString();
+            if (!order.equals("exit")) {
+                try {
+
+                    Dish dish = Dish.valueOf(order);
+                    dishList.add(dish);
+                } catch (IllegalArgumentException e) {
+                    writeMessage("Такого блюда нет");
+                }
+            }
+        }
+        ConsoleHelper.writeMessage("Ожидайте вашего заказа");
+        return dishList;
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -1,12 +1,28 @@
 package com.javarush.task.task27.task2712;
+import com.javarush.task.task27.task2712.kitchen.Order;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Tablet {
     private final int number;
+    private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
-    public void createOrder(){
 
+    @Override
+    public String toString() {
+        return "Tablet{number=" + number+ "}";
+    }
+
+    public void createOrder(){
+        try {
+            ConsoleHelper.writeMessage(new Order(this).toString());
+        }catch (IOException e){
+            logger.log(Level.SEVERE,"Console is unavailable.");
+        }
     }
 }
