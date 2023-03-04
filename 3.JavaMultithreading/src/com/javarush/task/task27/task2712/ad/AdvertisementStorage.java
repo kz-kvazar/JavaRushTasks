71439537eea45fce0d2d1f0f7abe51167ca59e23
kz-kvazar <milniy87@gmail.com>
2023-03-04 14:04:13 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,14 +1,31 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AdvertisementStorage {
-    private static class InstanceHolder{
-        private static final AdvertisementStorage storage = new AdvertisementStorage();
+    private static class InstanceHolder {
+        private static final AdvertisementStorage ourInstance = new AdvertisementStorage();
     }
 
-    private AdvertisementStorage() {
+    public static AdvertisementStorage getInstance() {
+        return InstanceHolder.ourInstance;
     }
-    
-    public static AdvertisementStorage getInstance(){
-        return InstanceHolder.storage;
+
+    private final List<Advertisement> videos = new ArrayList<>();
+
+    private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
+    }
+       public List<Advertisement> list(){
+        return videos;
+    }
+    public void add(Advertisement advertisement){
+        videos.add(advertisement);
+
     }
 }
