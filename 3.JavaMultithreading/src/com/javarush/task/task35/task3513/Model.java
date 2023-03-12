9d0136dd461ad133da2977209d0c63958a3f7b20
kz-kvazar <milniy87@gmail.com>
2023-03-12 09:01:59 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,45 +1,103 @@
 package com.javarush.task.task35.task3513;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Model {
     private Tile[][] gameTiles;
     private static final int FIELD_WIDTH = 4;
+    int maxTile = 0;
+    int score = 0;
 
     public Model() {
         resetGameTiles();
     }
 
-    private List<Tile> getEmptyTiles() {
-        List<Tile> emptyTile = new ArrayList<>();
-        for (int i = 0; i < gameTiles.length; i++) {
-            for (int j = 0; j < gameTiles[i].length; j++) {
-                if (gameTiles[i][j].isEmpty()) {
-                    emptyTile.add(gameTiles[i][j]);
-                }
-            }
-        }
-        return emptyTile;
-    }
-
-    private void addTile() {
-        int tileValue = Math.random() < 0.9 ? 2 : 4;
-        List<Tile> emptyTile = getEmptyTiles();
-        if (!emptyTile.isEmpty()) {
-            Tile randomTile = emptyTile.get((int) (Math.random() * emptyTile.size()));
-            randomTile.value = tileValue;
-        }
-    }
-
-    public void resetGameTiles() {
+    void resetGameTiles() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
-        for (int i = 0; i < gameTiles.length; i++) {
-            for (int j = 0; j < gameTiles[i].length; j++) {
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
         addTile();
         addTile();
+    }
+
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        if (!emptyTiles.isEmpty()) {
+            int index = (int) (Math.random() * emptyTiles.size()) % emptyTiles.size();
+            Tile emptyTile = emptyTiles.get(index);
+            emptyTile.value = Math.random() < 0.9 ? 2 : 4;
+        }
+    }
+
+    private List<Tile> getEmptyTiles() {
+        final List<Tile> list = new ArrayList<Tile>();
+        for (Tile[] tileArray : gameTiles) {
+            for (Tile t : tileArray)
+                if (t.isEmpty()) {
+                    list.add(t);
+                }
+        }
+        return list;
+    }
+    private boolean compressTiles(Tile[] tiles){
+        boolean hasChanged = false;
+        int insertPosition = 0;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (!tiles[i].isEmpty()){
+                if(i != insertPosition) {
+                    tiles[insertPosition] = tiles[i];
+                    tiles[i] = new Tile();
+                    hasChanged =true;
+                }
+                insertPosition++;
+            }
+        }
+        return hasChanged;
+    }
+
+    private boolean mergeTiles(Tile[] tiles) {
+        boolean hasChanged = false;
+        LinkedList<Tile> tilesList = new LinkedList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (tiles[i].isEmpty()) {
+                continue;
+            }
+
+            if (i < FIELD_WIDTH - 1 && tiles[i].value == tiles[i + 1].value) {
+                int updatedValue = tiles[i].value * 2;
+                if (updatedValue > maxTile) {
+                    maxTile = updatedValue;
+                }
+                score += updatedValue;
+                tilesList.addLast(new Tile(updatedValue));
+                tiles[i + 1].value = 0;
+                hasChanged = true;
+            } else {
+                tilesList.addLast(new Tile(tiles[i].value));
+            }
+            tiles[i].value = 0;
+        }
+
+        for (int i = 0; i < tilesList.size(); i++) {
+            tiles[i] = tilesList.get(i);
+        }
+        return hasChanged;
+    }
+
+    public void left() {
+        boolean moveFlag = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (compressTiles(gameTiles[i]) | mergeTiles(gameTiles[i])) {
+                moveFlag = true;
+            }
+        }
+        if (moveFlag) {
+            addTile();
+        }
     }
 }
