94c73f249851d00612b0fc3263fc13fce99f02d7
kz-kvazar <milniy87@gmail.com>
2023-03-12 19:31:40 +0200

    test

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,10 +1,41 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
     private Model model;
     private View view;
+    private static final int WINNING_TILE = 2048;
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        int keyCode = keyEvent.getKeyCode();
+        if (keyCode == KeyEvent.VK_ESCAPE){
+            resetGame();
+        } else if (!model.canMove()) {
+            view.isGameLost = true;
+        }if (!view.isGameLost && !view.isGameWon){
+            if ( keyCode == KeyEvent.VK_LEFT){
+                model.left();
+            } else if (keyCode == KeyEvent.VK_RIGHT) {
+                model.right();
+            } else if (keyCode == KeyEvent.VK_UP) {
+                model.up();
+            } else if (keyCode == KeyEvent.VK_DOWN) {
+                model.down();
+            }
+        }
+        if (model.maxTile == WINNING_TILE){
+            view.isGameWon = true;
+        }
+        view.repaint();
+    }
+
+    public Controller(Model model) {
+        this.model = model;
+        this.view = new View(this);
+    }
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -12,5 +43,11 @@ public class Controller extends KeyAdapter {
     
     public int getScore(){
         return model.score;
+    }
+    public void resetGame(){
+        model.score = 0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
     }
 }
