5d30cfc60acbfdce8fbf548f66cf1cc35c50b7d3
kz-kvazar <milniy87@gmail.com>
2023-03-18 15:52:08 +0200

    test

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -22,7 +22,18 @@ public class Field {
     public int[][] getMatrix() {
         return matrix;
     }
-    public void print(){}
+    public void print(){
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 0)
+                    System.out.print(".");
+                else
+                    System.out.print("X");
+                }
+                System.out.println();
+            }
+    }
     public void removeFullLines(){}
     public Integer getValue(int x, int y){
         return null;
