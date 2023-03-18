c19b9b2c3d4394e2cd57085dad14a4bb2577fe2a
kz-kvazar <milniy87@gmail.com>
2023-03-18 15:04:45 +0200

    test

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -1,7 +1,17 @@
 package com.javarush.task.task22.task2213;
 
 public class Tetris {
+    private Field field;
+    private Figure figure;
     public static void main(String[] args) {
+        
+    }
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
     }
 }
