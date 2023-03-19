224e1a819fcdc7dc1e8ab0fa0897d87bebe4f067
kz-kvazar <milniy87@gmail.com>
2023-03-19 09:49:00 +0200

    test

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -22,10 +22,30 @@ public class Figure {
     public int[][] getMatrix() {
         return matrix;
     }
-    public void left(){}
-    public void right(){}
-    public void down(){}
-    public void up(){}
+    public void left(){
+        x -= 1;
+        if (!isCurrentPositionAvailable()){
+            x += 1;
+        }
+    }
+    public void right(){
+        x += 1;
+        if (!isCurrentPositionAvailable()){
+            x -= 1;
+        }
+    }
+    public void down(){
+        y += 1;
+        if (!isCurrentPositionAvailable()){
+            y -= 1;
+        }
+    }
+    public void up(){
+        y -= 1;
+        if (!isCurrentPositionAvailable()){
+            y += 1;
+        }
+    }
     public void rotate(){}
     public void downMaximum(){}
     public boolean isCurrentPositionAvailable(){
