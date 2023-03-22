e64c150425c35a3689bedd6b371db2b58b80f360
kz-kvazar <milniy87@gmail.com>
2023-03-22 12:46:01 +0200

    test

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -42,4 +42,17 @@ public class Canvas {
             }
         }
     }
+    public void clear(){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+    public void print(){
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(matrix[i]);
+        }
+        System.out.println();
+    }
 }
