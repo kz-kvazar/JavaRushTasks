a06317fdfb295d503c02ee315a684bf1344ea706
kz-kvazar <milniy87@gmail.com>
2023-03-27 17:18:03 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -5,15 +5,26 @@ import java.util.*;
 
 public class AmigoSet<E> extends AbstractSet<E> implements  Set<E>, Serializable, Cloneable {
 private static final Object PRESENT = new Object();
-private transient HashMap<E, Object> map;
+private transient HashMap<E,Object> map;
 
     public AmigoSet() {
-        this.map = new HashMap<>();
+        map = new HashMap<>();
     }
 
     public AmigoSet(Collection<? extends E> collection) {
         this.map = new HashMap<>(Math.max((int) (collection.size() / .75f) + 1, 16));
         addAll(collection);
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch (Exception e) {
+            throw new InternalError();
+        }
     }
 
     @Override
@@ -23,7 +34,7 @@ private transient HashMap<E, Object> map;
 
     @Override
     public Iterator<E> iterator() {
-        return map.keySet().iterator();
+        return  map.keySet().iterator();
     }
 
     @Override
