6da11b7c2d490dd42cf7511bf8412f1426cd34c3
kz-kvazar <milniy87@gmail.com>
2023-03-27 17:53:27 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.*;
 
@@ -16,25 +19,13 @@ private transient HashMap<E,Object> map;
         addAll(collection);
     }
 
-    @Override
-    public Object clone() {
-        try {
-            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
-            newSet.map = (HashMap<E, Object>) map.clone();
-            return newSet;
-        } catch (Exception e) {
-            throw new InternalError();
-        }
-    }
-
-    @Override
     public boolean add(E e) {
         return map.put(e, PRESENT) == null;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return  map.keySet().iterator();
+        return map.keySet().iterator();
     }
 
     @Override
@@ -53,12 +44,57 @@ private transient HashMap<E,Object> map;
     }
 
     @Override
-    public boolean remove(Object o) {
-        return map.remove(o) == null;
+    public void clear() {
+        map.clear();
     }
 
     @Override
-    public void clear() {
-        map.clear();
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+    @Override
+    public Object clone() {
+        try {
+            AmigoSet<E> newSet = (AmigoSet<E>) super.clone();
+            newSet.map = (HashMap<E, Object>) map.clone();
+            return newSet;
+        } catch (Exception e) {
+            throw new InternalError();
+        }
+    }
+
+    private void writeObject(ObjectOutputStream s) throws java.io.IOException {
+        // Write out any hidden serialization magic
+        s.defaultWriteObject();
+
+        // Write out HashMap capacity and load factor
+        s.writeInt(HashMapReflectionHelper.<Integer>callHiddenMethod(map, "capacity"));
+        s.writeFloat(HashMapReflectionHelper.<Float>callHiddenMethod(map, "loadFactor"));
+
+        // Write out size
+        s.writeInt(map.size());
+
+        // Write out all elements in the proper order.
+        for (E e : map.keySet())
+            s.writeObject(e);
+    }
+
+    private void readObject(ObjectInputStream s) throws java.io.IOException, ClassNotFoundException {
+        // Read in any hidden serialization magic
+        s.defaultReadObject();
+
+        // Read in HashMap capacity and load factor and create backing HashMap
+        int capacity = s.readInt();
+        float loadFactor = s.readFloat();
+        map = new HashMap<>(capacity, loadFactor);
+
+        // Read in size
+        int size = s.readInt();
+
+        // Read in all elements in the proper order.
+        for (int i = 0; i < size; i++) {
+            E e = (E) s.readObject();
+            map.put(e, PRESENT);
+        }
     }
 }
