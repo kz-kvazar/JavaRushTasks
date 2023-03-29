1e33c279687a3058cf45fce1941f785c277132be
kz-kvazar <milniy87@gmail.com>
2023-03-29 20:27:44 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,7 +1,16 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 public class Solution {
     public static void main(String[] args) {
+        MaleFactory maleFactory = new MaleFactory();
+        Human man = maleFactory.getPerson(99);
+        Human kid = maleFactory.getPerson(4);
+        Human teen = maleFactory.getPerson(15);
 
+        System.out.println(man);
+        System.out.println(kid);
+        System.out.println(teen);
     }
 }
