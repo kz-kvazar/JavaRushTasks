1f7261a10f54a2a4d6f81ef9afa56f931663f384
kz-kvazar <milniy87@gmail.com>
2023-03-29 20:47:44 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,16 +1,18 @@
 package com.javarush.task.task37.task3702;
 
-import com.javarush.task.task37.task3702.male.MaleFactory;
-
 public class Solution {
     public static void main(String[] args) {
-        MaleFactory maleFactory = new MaleFactory();
-        Human man = maleFactory.getPerson(99);
-        Human kid = maleFactory.getPerson(4);
-        Human teen = maleFactory.getPerson(15);
+        AbstractFactory factory = FactoryProducer.getFactory(FactoryProducer.HumanFactoryType.FEMALE);
+        useFactory(factory);
 
-        System.out.println(man);
-        System.out.println(kid);
-        System.out.println(teen);
+        factory = FactoryProducer.getFactory(FactoryProducer.HumanFactoryType.MALE);
+        useFactory(factory);
     }
+
+    public static void useFactory(AbstractFactory factory) {
+        System.out.println(factory.getPerson(99));
+        System.out.println(factory.getPerson(4));
+        System.out.println(factory.getPerson(15));
+    }
+
 }
