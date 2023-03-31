a7fa5026f32247ac8a7fb9b8557734f63729a311
kz-kvazar <milniy87@gmail.com>
2023-03-31 13:17:47 +0300

    test

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3602;
+
+import java.util.Collections;
+
+
+/* 
+Найти класс по описанию Ӏ Java Collections: 6 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        Class<?>[] declaredClasses = Collections.class.getDeclaredClasses();
+        Class result = null;
+        for (Class c : declaredClasses) {
+            if (c.getSimpleName().equals("EmptyList")){
+                result = c;
+            }
+        }
+        return result;
+    }
+
+}
