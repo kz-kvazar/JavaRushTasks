adf1aa16d631f0e8a402fd2cffc8331aac07d6eb
kz-kvazar <milniy87@gmail.com>
2023-06-18 11:33:00 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,7 +1,59 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) {
+        HashMapStorageStrategy strategy = new HashMapStorageStrategy();
+        testStrategy(strategy,10000);
+    }
 
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber){
+        Helper.printMessage(strategy.getClass().getSimpleName() + ":");
+
+        Set<String> originalStrings = new HashSet<>();
+        for (int i = 0; i < elementsNumber; ++i) {
+            originalStrings.add(Helper.generateRandomString());
+        }
+
+        Shortener shortener = new Shortener(strategy);
+
+        Date start = new Date();
+        Set<Long> ids = getIds(shortener, originalStrings);
+        Date stop = new Date();
+        String result = String.valueOf(stop.getTime() - start.getTime());
+        Helper.printMessage("Время получения идентификаторов для " + elementsNumber + " строк: " + result);
+
+        start = new Date();
+        Set<String> strings = getStrings(shortener, ids);
+        stop = new Date();
+        result = String.valueOf(stop.getTime() - start.getTime());
+        Helper.printMessage("Время получения строк для " + elementsNumber + " идентификаторов: " + result);
+
+        if (originalStrings.equals(strings)){
+            Helper.printMessage("Тест пройден.");
+        } else {
+            Helper.printMessage("Тест не пройден.");
+        }
+        Helper.printMessage("");
+    }
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings){
+        Set<Long> set = new HashSet<>();
+        for (String s : strings) {
+            set.add(shortener.getId(s));
+        }
+        return set;
+    }
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys){
+        Set<String> set = new HashSet<>();
+        for (Long n : keys) {
+            set.add(shortener.getString(n));
+        }
+        return  set;
     }
 }
