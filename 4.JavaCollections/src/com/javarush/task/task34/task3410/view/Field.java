8ce15407ea878bb87d4153b0cba5bfdb045fc4a4
kz-kvazar <milniy87@gmail.com>
2023-08-13 16:23:14 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 public class Field extends JPanel {
     private final View view;
@@ -12,7 +14,14 @@ public class Field extends JPanel {
     public Field(View view) {
         this.view = view;
     }
-    public void paint(Graphics g){}
+    public void paint(Graphics g){
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        Set<GameObject> all = view.getGameObjects().getAll();
+        for (GameObject obj : all) {
+            obj.draw(g);
+        }
+    }
     public void setEventListener(EventListener eventListener){
         this.eventListener = eventListener;
     }
