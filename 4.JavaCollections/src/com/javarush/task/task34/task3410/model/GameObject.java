caf70dec9fb09fd66ee068c71ee2496f1924defc
kz-kvazar <milniy87@gmail.com>
2023-08-13 13:15:37 +0300

    test

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObject.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObject.java
@@ -3,8 +3,8 @@ package com.javarush.task.task34.task3410.model;
 import java.awt.*;
 
 public abstract class GameObject {
-    private int x;
-    private int y;
+    protected int x;
+    protected int y;
     private int width;
     private int height;
 
